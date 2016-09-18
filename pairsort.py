e790bc7f59a8b15dfda65c8177e14c5d5d513416
IrinaNizova <alisher_inn@mail.ru>
2016-09-18 16:54:08 +0300

    fix pairsort

--- pairsort.py
+++ pairsort.py
@@ -1,14 +1,20 @@
 from itertools import permutations
 
 def pairsort(lst):
-    switch = True
-    while switch:
-        switch = False
-        for i in range(0, len(lst)-3):
-            if lst[i]*10+lst[i+1] > lst[i+2]*10+lst[i+3]:
-                lst[i:i+2], lst[i+2: i+4] = lst[i+2: i+4], lst[i:i+2]
-                switch = True
+    for i in range(0, len(lst)-3):
+        curr_pair = num_from_lst(lst[i:i+2])
+        min_pair_index = i
+        for j in range(i+2, len(lst)-1):
+            if lst[j]*10+lst[j+1] < curr_pair:
+                min_pair_index = j
+                curr_pair = num_from_lst(lst[j:j+2])
+        if min_pair_index != i:
+            lst[i:i+2], lst[min_pair_index:min_pair_index+2] = \
+                lst[min_pair_index:min_pair_index+2], lst[i:i+2]
     return lst
+
+def num_from_lst(lst):
+    return sum(l*10**i for i, l in enumerate(lst[::-1]))
 
 def is_sorted(lst):
     return False if any(1 for i in range(len(lst)-1) if lst[i]>lst[i+1]) else True
@@ -20,6 +26,7 @@ if __name__ == "__main__":
     not_sorted = 0
     for lst in permutations([1,2,3,4,5,6]):
         if is_sorted(pairsort(list(lst))):
+            print(lst)
             sorted += 1
         else:
             not_sorted += 1
